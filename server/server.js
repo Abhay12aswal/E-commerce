044a50a66eb47ef91ead2b0ca044edf26c09e7a9
Abhay12aswal <abhayaswal939@gmail.com>
2024-09-14 11:07:56 +0530

    routing & controllers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
-const express= require('express')
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
+// const adminProductsRouter = require("./routes/admin/products-routes");
+// const adminOrderRouter = require("./routes/admin/order-routes");
+
+// const shopProductsRouter = require("./routes/shop/products-routes");
+// const shopCartRouter = require("./routes/shop/cart-routes");
+// const shopAddressRouter = require("./routes/shop/address-routes");
+// const shopOrderRouter = require("./routes/shop/order-routes");
+// const shopSearchRouter = require("./routes/shop/search-routes");
+// const shopReviewRouter = require("./routes/shop/review-routes");
+
+// const commonFeatureRouter = require("./routes/common/feature-routes");
 
 mongoose.connect(
     'mongodb+srv://abhayaswal99:zyIPEBSR7ZJWSHKS@cluster0.x4a7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
@@ -28,6 +40,20 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
+
+app.use("/api/auth", authRouter);
+// app.use("/api/admin/products", adminProductsRouter);
+// app.use("/api/admin/orders", adminOrderRouter);
+
+// app.use("/api/shop/products", shopProductsRouter);
+// app.use("/api/shop/cart", shopCartRouter);
+// app.use("/api/shop/address", shopAddressRouter);
+// app.use("/api/shop/order", shopOrderRouter);
+// app.use("/api/shop/search", shopSearchRouter);
+// app.use("/api/shop/review", shopReviewRouter);
+
+// app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(PORT,()=>{
     console.log('server is listening in 5000 ports')
